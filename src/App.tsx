aee3ca232b70e775629a857a2faac6882547c30e
Keegan Wolf <kwolf313@live.com>
2022-09-27 21:05:38 -0500

    Got the math steps working and the input fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [stage, setStage] = useState(2);
+  const [multiplyer, setMulti] = useState(2);
+  const [userAns, setUserAns] = useState(0);
+
+  const userSubmit = (e: any) => {
+    e.preventDefault();
+
+    console.log(userAns,'submitted!');
+
+    if (stage * multiplyer !== userAns) {
+      console.log('Please try again.');
+      setUserAns(0);
+
+
+      return;
+    }
+
+    console.log('Correct!');
+
+    if (stage === 10) {
+      setStage(2);
+      setMulti(multiplyer+1);
+
+    } else {
+      setStage(stage+1);
+
+    }
+
+
+    setUserAns(0);
+  }
+
   return (
     <div className="App">
+      <div>
+        What is { stage } x { multiplyer }?
 
+        <form onSubmit={ userSubmit }>
+
+          <input
+          type="number"
+          value={ userAns === 0 ? '' : userAns }
+          onChange={ (e) => { setUserAns(Number(e.target.value)) } }
+          />
+
+          <input
+          type="submit"
+          />
+
+        </form>
+      </div>
     </div>
   );
 }
